7d5e1304b636f3c79880b1d9f1dfab085cc958ce
Samuele De Cristofaro <164782142+Samuele-DeCristofaro@users.noreply.github.com>
2025-03-31 07:43:24 +0200

    Add files via upload
    Signed-off-by: Samuele De Cristofaro <164782142+Samuele-DeCristofaro@users.noreply.github.com>

---
+++ RegisterInterface/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.register2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button access;
+
+    @FXML
+    public void initialize() {
+        // Imposta il testo di benvenuto al caricamento dell'app
+    }
+}
